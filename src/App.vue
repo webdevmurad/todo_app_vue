8f5498eb3fa78a6aee84aeef4be9475910ed07a7
Murad <murad-arslangiraev-1994@mail.ru>
2020-10-31 19:19:01 +0300

    add task and completed task

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,54 @@
         div.todo
             div.todo__header
                 h4 Список задач
-            div.todo__app-field
-                input.todo__app-field-input(
-                    type='text'
-                    placeholder="Введите текст задачи..."
+            TaskField(
+                @onAddTask="onAddTask"
+            )
+            div.todo-list
+                ListItem(
+                    v-for="(task, index) in tasks" 
+                    :key="index" 
+                    :text="task.text" 
+                    :completed="task.completed"
+                    :index="index"
+                    @onToggleCompleted="onToggleCompleted"
                 )
-                button.todo__app-field-button
-                    svg.todo__app-field-plus(height="20px" viewBox="0 0 469.33333 469.33333" width="20px" xmlns="http://www.w3.org/2000/svg")
-                        path(d="m437.332031 192h-160v-160c0-17.664062-14.335937-32-32-32h-21.332031c-17.664062 0-32 14.335938-32 32v160h-160c-17.664062 0-32 14.335938-32 32v21.332031c0 17.664063 14.335938 32 32 32h160v160c0 17.664063 14.335938 32 32 32h21.332031c17.664063 0 32-14.335937 32-32v-160h160c17.664063 0 32-14.335937 32-32v-21.332031c0-17.664062-14.335937-32-32-32zm0 0")
-            div.todo__list-items
-                div.todo__list-left-block 
-                    div.todo__list-item-check
-                        svg(width="100%" height="100%" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve")              
-                            path(d="M497.36,69.995c-7.532-7.545-19.753-7.558-27.285-0.032L238.582,300.845l-83.522-90.713 c-7.217-7.834-19.419-8.342-27.266-1.126c-7.841,7.217-8.343,19.425-1.126,27.266l97.126,105.481 c3.557,3.866,8.535,6.111,13.784,6.22c0.141,0.006,0.277,0.006,0.412,0.006c5.101,0,10.008-2.026,13.623-5.628L497.322,97.286 C504.873,89.761,504.886,77.54,497.36,69.995z")
-                            path(d="M492.703,236.703c-10.658,0-19.296,8.638-19.296,19.297c0,119.883-97.524,217.407-217.407,217.407 c-119.876,0-217.407-97.524-217.407-217.407c0-119.876,97.531-217.407,217.407-217.407c10.658,0,19.297-8.638,19.297-19.296 C275.297,8.638,266.658,0,256,0C114.84,0,0,114.84,0,256c0,141.154,114.84,256,256,256c141.154,0,256-114.846,256-256 C512,245.342,503.362,236.703,492.703,236.703z")
-                    p.todo__list-item-text Текст
-                div.todo__list-right-block 
-                    div.todo__list-item-remove
-                        svg(height="20px" viewBox="-40 0 427 427.00131" width="20px" xmlns="http://www.w3.org/2000/svg")
-                            path(d="m232.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0")
-                            path(d="m114.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0")
-                            path(d="m28.398438 127.121094v246.378906c0 14.5625 5.339843 28.238281 14.667968 38.050781 9.285156 9.839844 22.207032 15.425781 35.730469 15.449219h189.203125c13.527344-.023438 26.449219-5.609375 35.730469-15.449219 9.328125-9.8125 14.667969-23.488281 14.667969-38.050781v-246.378906c18.542968-4.921875 30.558593-22.835938 28.078124-41.863282-2.484374-19.023437-18.691406-33.253906-37.878906-33.257812h-51.199218v-12.5c.058593-10.511719-4.097657-20.605469-11.539063-28.03125-7.441406-7.421875-17.550781-11.5546875-28.0625-11.46875h-88.796875c-10.511719-.0859375-20.621094 4.046875-28.0625 11.46875-7.441406 7.425781-11.597656 17.519531-11.539062 28.03125v12.5h-51.199219c-19.1875.003906-35.394531 14.234375-37.878907 33.257812-2.480468 19.027344 9.535157 36.941407 28.078126 41.863282zm239.601562 279.878906h-189.203125c-17.097656 0-30.398437-14.6875-30.398437-33.5v-245.5h250v245.5c0 18.8125-13.300782 33.5-30.398438 33.5zm-158.601562-367.5c-.066407-5.207031 1.980468-10.21875 5.675781-13.894531 3.691406-3.675781 8.714843-5.695313 13.925781-5.605469h88.796875c5.210937-.089844 10.234375 1.929688 13.925781 5.605469 3.695313 3.671875 5.742188 8.6875 5.675782 13.894531v12.5h-128zm-71.199219 32.5h270.398437c9.941406 0 18 8.058594 18 18s-8.058594 18-18 18h-270.398437c-9.941407 0-18-8.058594-18-18s8.058593-18 18-18zm0 0")
-                            path(d="m173.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0")
-
 </template>
 
 <script>
 
+import ListItem from '@/components/ListItem';
+import TaskField from '@/components/TaskField';
+
 export default {
     name: 'App',
+    data: () => ({
+        tasks: [
+            {
+                text: 'Изучить VueJS',
+                completed: false
+            },
+            {
+                text: 'Изучить ReactJS',
+                completed: true
+            }
+        ]
+    }),
+    methods: {
+        onToggleCompleted(index) {
+            this.tasks[index].completed = !this.tasks[index].completed
+        },
+        onAddTask(text) {
+            console.log(text)
+            this.tasks.push({
+                text,
+                completed: false
+            })
+        }
+    },
     components: {
+        ListItem,
+        TaskField
     }
 }
 </script>
@@ -102,12 +120,18 @@ export default {
         &-left
             &-block 
                 display: flex
+                width: 100%
+                &:hover 
+                    cursor: pointer
         &-items 
             display: flex
             justify-content: space-between
-
+            align-items: center
+            margin-bottom: 20px
             padding-left: 20px 
             padding-right: 20px
+
+            
         &-item
             &-check
                 margin-right: 20px
@@ -116,17 +140,9 @@ export default {
                 border-radius: 50%
                 border: 1px solid #259CD8
                 overflow: hidden
-                transition: .5s
-                &:hover 
-                    cursor: pointer
-                    border: none
-                    transition: .5s
-                &:hover svg 
-                    transition: .5s
-                    fill: #259CD8
+                transition: .3s
                 svg 
                     fill: white
-                    transition: .1s
             &-text 
                 font-size: 20px
             &-remove
@@ -135,6 +151,17 @@ export default {
                 &:hover svg
                     cursor: pointer
                     fill: red
+    &-list 
+        &__completed 
+            .todo__list-item-check svg 
+                transition: 0.3s
+                fill: #259CD8
+            .todo__list-item-check
+                border: 1px solid white
+            .todo__list-item-text
+                transition: 0.3s
+                text-decoration: line-through
+
 
 
 
